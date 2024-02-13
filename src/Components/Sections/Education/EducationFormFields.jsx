266a51654091609d5f87a92f77415a5b5ff8fbbc
Jeanne <j.pietropaoli@yahoo.fr>
2024-02-13 22:04:56 +0100

    Update form structure

--- src/Components/Sections/Education/EducationFormFields.jsx
+++ src/Components/Sections/Education/EducationFormFields.jsx
@@ -1,20 +1,31 @@
 export default function EducationFormFields(props) {
     return (
-        <ul className="form-fields">
-            {props.temporaryData.map(course => {
-                return (
-                    <li key={course.id} id={course.id}>
-                        <label htmlFor={`course${course.id}-school`}>School</label>
-                        <input type="text" name="school" id={`course${course.id}-school`} value={course.school} onChange={(e) => props.handleTemporaryDataChange(e)} />
-                        <label htmlFor={`course${course.id}-degree`}>Degree</label>
-                        <input type="text" name="degree" id={`course${course.id}-degree`} value={course.degree} onChange={(e) => props.handleTemporaryDataChange(e)} />
-                        <label htmlFor={`course${course.id}-start-date`}>Start date</label>
-                        <input type="number" name="startDate" id={`course${course.id}-start-date`} value={course.startDate} onChange={(e) => props.handleTemporaryDataChange(e)} />
-                        <label htmlFor={`course${course.id}-end-date`}>End date</label>
-                        <input type="number" name="endDate" id={`course${course.id}-end-date`} value={course.endDate} onChange={(e) => props.handleTemporaryDataChange(e)} />
-                        <label htmlFor={`course${course.id}-description`}>Description</label>
-                        <input type="text" name="description" id={`course${course.id}-description`} value={course.description} onChange={(e) => props.handleTemporaryDataChange(e)} />
-                    </li>
+        <ul>
+            {props.temporaryData.map((course, index) => {
+                 return (                  
+                    <ul className="form-fields" key={course.id} id={course.id}>
+                        <h3>Course {index + 1}</h3>
+                        <li>
+                            <label htmlFor={`course${course.id}-school`}>School</label>
+                            <input type="text" name="school" id={`course${course.id}-school`} value={course.school} onChange={(e) => props.handleTemporaryDataChange(e)} />
+                        </li>
+                        <li>
+                            <label htmlFor={`course${course.id}-degree`}>Degree</label>
+                            <input type="text" name="degree" id={`course${course.id}-degree`} value={course.degree} onChange={(e) => props.handleTemporaryDataChange(e)} />
+                        </li>
+                        <li>
+                            <label htmlFor={`course${course.id}-start-date`}>Start date</label>
+                            <input type="number" name="startDate" id={`course${course.id}-start-date`} value={course.startDate} onChange={(e) => props.handleTemporaryDataChange(e)} />
+                        </li>
+                        <li>
+                            <label htmlFor={`course${course.id}-end-date`}>End date</label>
+                            <input type="number" name="endDate" id={`course${course.id}-end-date`} value={course.endDate} onChange={(e) => props.handleTemporaryDataChange(e)} />
+                        </li>
+                        <li>
+                            <label htmlFor={`course${course.id}-description`}>Description</label>
+                            <textarea type="text" name="description" id={`course${course.id}-description`} value={course.description} onChange={(e) => props.handleTemporaryDataChange(e)} />
+                        </li>
+                    </ul>
                 )
             })}
         </ul>
--- src/Components/Sections/Experiences/ExperiencesFormFields.jsx
+++ src/Components/Sections/Experiences/ExperiencesFormFields.jsx
@@ -1,20 +1,31 @@
 export default function ExperiencesFormFields(props) {
     return (
-        <ul className="form-fields">
-            {props.temporaryData.map(experience => {
+        <ul>
+            {props.temporaryData.map((experience, index) => {
                 return (
-                    <li key={experience.id} id={experience.id}>
-                        <label htmlFor={`experience${experience.id}-title`}>Title</label>
-                        <input type="text" name="title" id={`experience${experience.id}-title`} value={experience.title} onChange={(e) => props.handleTemporaryDataChange(e)} />
-                        <label htmlFor={`experience${experience.id}-company`}>Company</label>
-                        <input type="text" name="company" id={`experience${experience.id}-company`} value={experience.company} onChange={(e) => props.handleTemporaryDataChange(e)} />
-                        <label htmlFor={`experience${experience.id}-start-date`}>Start date</label>
-                        <input type="number" name="startDate" id={`experience${experience.id}-start-date`} value={experience.startDate} onChange={(e) => props.handleTemporaryDataChange(e)} />
-                        <label htmlFor={`experience${experience.id}-end-date`}>End date</label>
-                        <input type="number" name="endDate" id={`experience${experience.id}-end-date`} value={experience.endDate} onChange={(e) => props.handleTemporaryDataChange(e)} />
-                        <label htmlFor={`experience${experience.id}-description`}>Description</label>
-                        <input type="text" name="description" id={`experience${experience.id}-description`} value={experience.description} onChange={(e) => props.handleTemporaryDataChange(e)} />
-                    </li>
+                    <ul className="form-fields" key={experience.id} id={experience.id}>
+                        <h3>Experience {index + 1}</h3>
+                        <li>
+                            <label htmlFor={`experience${experience.id}-title`}>Title</label>
+                            <input type="text" name="title" id={`experience${experience.id}-title`} value={experience.title} onChange={(e) => props.handleTemporaryDataChange(e)} />
+                        </li>
+                        <li>
+                            <label htmlFor={`experience${experience.id}-company`}>Company</label>
+                            <input type="text" name="company" id={`experience${experience.id}-company`} value={experience.company} onChange={(e) => props.handleTemporaryDataChange(e)} />
+                        </li>
+                        <li>
+                            <label htmlFor={`experience${experience.id}-start-date`}>Start date</label>
+                            <input type="number" name="startDate" id={`experience${experience.id}-start-date`} value={experience.startDate} onChange={(e) => props.handleTemporaryDataChange(e)} />
+                        </li>
+                        <li>
+                            <label htmlFor={`experience${experience.id}-end-date`}>End date</label>
+                            <input type="number" name="endDate" id={`experience${experience.id}-end-date`} value={experience.endDate} onChange={(e) => props.handleTemporaryDataChange(e)} />
+                        </li>
+                        <li>
+                            <label htmlFor={`experience${experience.id}-description`}>Description</label>
+                            <textarea type="text" name="description" id={`experience${experience.id}-description`} value={experience.description} onChange={(e) => props.handleTemporaryDataChange(e)} />
+                        </li>
+                    </ul>
                 )
             })}
         </ul>
