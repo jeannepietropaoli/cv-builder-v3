76b051db8dc8cfa71c23db4f855fdca003972a30
Jeanne <j.pietropaoli@yahoo.fr>
2024-02-13 22:11:32 +0100

    Update form structure

--- src/Components/Sections/Introduction/IntroductionFormFields.jsx
+++ src/Components/Sections/Introduction/IntroductionFormFields.jsx
@@ -1,8 +1,10 @@
 export default function IntroductionFormFields(props) {
     return (
-        <div className="form-fields">
-            <label htmlFor="introduction-text"></label>
-            <input type="text" name="introduction-text" id="introduction-text" value={props.temporaryData} onChange={(e) => props.handleTemporaryDataChange(e)} />
-        </div>
+        <ul className="form-fields">
+            <li>
+                <label htmlFor="introduction-text"></label>
+                <textarea type="text" name="introduction-text" id="introduction-text" value={props.temporaryData} onChange={(e) => props.handleTemporaryDataChange(e)} />
+            </li>
+        </ul>
     )
 }
